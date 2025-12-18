5653869602a1660c36ddf7133ab95e5b2f7a2fb0
Pranitha <pranithareddy160805@gmail.com>
2025-12-18 22:31:56 +0530

    Modifief file

--- api-gateway-flightbookingsystem/src/main/java/com/flightapp/demo/config/SecurityConfig.java
+++ api-gateway-flightbookingsystem/src/main/java/com/flightapp/demo/config/SecurityConfig.java
@@ -1,20 +1,40 @@
 package com.flightapp.demo.config;
 
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
-	 @Bean
-	    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-	        return http
-	            .csrf(ServerHttpSecurity.CsrfSpec::disable)
-	            .authorizeExchange(ex -> ex.anyExchange().permitAll())
-	            .build();
-	    }
-	}
 
+    @Bean
+    public SecurityWebFilterChain security(ServerHttpSecurity http) {
+        return http
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .authorizeExchange(ex -> ex
+            		.pathMatchers(HttpMethod.OPTIONS, "/**").permitAll() 
+                .pathMatchers("auth-service/auth/login", "auth-service/auth/signup", "auth-service/auth/ping").permitAll()
+                .anyExchange().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt())
+            .build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder(@Value("${jwt.secret}") String secret) {
+        SecretKey key = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        return NimbusReactiveJwtDecoder.withSecretKey(key).build();
+    }
+}
