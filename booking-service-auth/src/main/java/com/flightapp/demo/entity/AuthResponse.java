309ccacf01846f57578ff020c1208b7ee5726117
Pranitha <pranithareddy160805@gmail.com>
2025-12-18 22:30:12 +0530

    Added authResponse Entity

---
+++ booking-service-auth/src/main/java/com/flightapp/demo/entity/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.flightapp.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String id;
+    private String email;
+    private String username;
+    private String role;
+}
