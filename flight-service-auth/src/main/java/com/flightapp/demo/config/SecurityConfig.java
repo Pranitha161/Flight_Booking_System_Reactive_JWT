f58a06e5461e96b4ed619cf4d5b01402965b4ba8
Pranitha <pranithareddy160805@gmail.com>
2025-12-18 22:31:05 +0530

    Added config files in flight microservice

---
+++ flight-service-auth/src/main/java/com/flightapp/demo/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.flightapp.demo.config;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+	@Value("${jwt.secret}")
+	private String secret;
+
+	@Bean
+	public SecurityWebFilterChain security(ServerHttpSecurity http) {
+		return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+				.authorizeExchange(ex -> ex.pathMatchers("/auth/ping").permitAll().pathMatchers("/api/flight/internal/**").permitAll().pathMatchers("/api/seats/**").permitAll().anyExchange().authenticated())
+				.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtDecoder(jwtDecoder(secret)))).build();
+	}
+
+	@Bean
+	public ReactiveJwtDecoder jwtDecoder(@Value("${jwt.secret}") String secret) {
+	    return NimbusReactiveJwtDecoder.withSecretKey(
+	            new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256")
+	    ).build();
+	}
+
+}
