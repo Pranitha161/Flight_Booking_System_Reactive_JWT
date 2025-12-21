5c33c7cad97b143c1bc5ee32d151c620747734c9
Pranitha <pranithareddy160805@gmail.com>
2025-12-21 12:26:45 +0530

    Added global exception handlers

---
+++ flight-service-auth/src/main/java/com/flightapp/demo/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.flightapp.demo;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(WebExchangeBindException.class)
+	public ResponseEntity<Map<String, Object>> handleValidationExceptions(WebExchangeBindException ex) {
+		List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(error -> error.getDefaultMessage())
+				.collect(Collectors.toList());
+
+		Map<String, Object> body = new HashMap<>();
+		body.put("status", HttpStatus.BAD_REQUEST.value());
+		body.put("error", "Bad Request");
+		body.put("errors", errors);
+
+		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+	}
+
+}
